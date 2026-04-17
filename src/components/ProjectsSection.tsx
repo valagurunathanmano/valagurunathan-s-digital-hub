1175fe051096ecc484c096b6b977bea05caa6c35
Valagurunathan M <140380628+valagurunathanmano@users.noreply.github.com>
2026-04-17 13:32:01 +0530

    ProjectsSection.tsx

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -5,7 +5,7 @@ const projects = [
   {
     title: "Keyword Research Case Study",
     desc: "Developed a structured SEO keyword strategy by combining manual research, search intent classification, clustering, and data validation, supported by a documented strategy summary.",
-    Link:"",
+    Link:"https://docs.google.com/spreadsheets/d/1bHM9BczQP4sLIzxBayR8bCRsOWFpfqpNIJQ6AjU5nyg/edit?usp=sharing",
   },
   {
     title: "SEO Optimised Blog Article Case Study",
